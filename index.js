e41d01dfce610747b25efba95027886031bdc324
Sharif33 <sharif.cse.nu@gmail.com>
2021-12-16 23:22:39 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,203 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+require('dotenv').config()
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w273s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('mobileManagement');
+        const mobileCollection = database.collection('phones');
+        const usersCollection = database.collection('users');
+        const MyOrder = database.collection('orders');
+        const Reviews = database.collection('reviews');
+
+
+        // GET phones
+        app.get('/phones', async (req, res) => {
+            const cursor = mobileCollection.find({});
+            const phones = await cursor.toArray();
+            res.send(phones);
+        });
+
+
+        // DELETE phones from ManageProducts
+        app.delete('/phones/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await mobileCollection.deleteOne(query);
+            res.json(result);
+        });
+
+
+        // GET Single bike
+        app.get('/phones/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('getting specific service', id);
+            const query = { _id: ObjectId(id) };
+            const bike = await mobileCollection.findOne(query);
+            res.json(bike);
+        })
+
+        // POST bike
+        app.post('/phones', async (req, res) => {
+            const bike = req.body;
+            console.log('hit the post api', bike);
+            const result = await mobileCollection.insertOne(bike);
+            console.log(result);
+            res.json(result)
+        });
+
+        // GET Reviews
+        app.get('/reviews', async (req, res) => {
+            const cursor = Reviews.find({});
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+
+        // POST Review
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            console.log('hit the post api', review);
+            const result = await Reviews.insertOne(review);
+            console.log(result);
+            res.json(result)
+        });
+
+        // GET orders
+        app.get('/orders', async (req, res) => {
+            const cursor = MyOrder.find({});
+            const orders = await cursor.toArray();
+            res.send(orders);
+        });
+
+        // GET all order by email
+        app.get("/myOrders/:email", (req, res) => {
+            console.log(req.params);
+            MyOrder
+                .find({ email: req.params.email })
+                .toArray((err, results) => {
+                    res.send(results);
+                });
+        });
+
+        //DELETE my order
+        app.delete('/myOrders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await MyOrder.deleteOne(query);
+            res.json(result);
+        })
+
+        // POST orders
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            console.log('hit the post api', order);
+            const result = await MyOrder.insertOne(order);
+            console.log(result);
+            res.json(result)
+        });
+
+        // DELETE orders from ManageOrders
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await MyOrder.deleteOne(query);
+            res.json(result);
+        });
+
+
+        // user and admin part
+
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        });
+
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            console.log('put', user);
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+        });
+
+        //Update get
+        app.get('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const user = await MyOrder.findOne(query);
+            // console.log('load user with id: ', id);
+            res.send(user);
+        })
+
+        //  update
+        app.put("/updateStatus/:id", async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+
+            MyOrder
+                .updateOne(filter, {
+                    $set: {
+                        status: "Shipped"
+                    },
+                })
+                .then((result) => {
+                    res.send(result);
+                    console.log(result);
+                });
+
+        });
+
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Network Server is Runnning')
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
