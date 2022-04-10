07bef7b4cc6b4f5d2d28e8837549e365c6a4f259
Sharif33 <sharif.cse.nu@gmail.cm>
2022-04-10 13:34:49 +0600

    file upload

--- index.js
+++ index.js
@@ -16,6 +16,31 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 app.use(cors());
 app.use(express.json());
 
+// for image upload
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
+let path = require('path');
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'images');
+    },
+    filename: function(req, file, cb) {   
+        cb(null, uuidv4() + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    const allowedFileTypes = ['image/jpeg', 'image/jpg', 'image/png'];
+    if(allowedFileTypes.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
+let upload = multer({ storage, fileFilter });
+
 async function run() {
     try {
         await client.connect();
@@ -97,7 +122,7 @@ async function run() {
         })
 
         // POST phone
-        app.post('/phones', async (req, res) => {
+        app.post('/phones',upload.single('photo'), async (req, res) => {
             const bike = req.body;
             console.log('hit the post api', bike);
             const result = await mobileCollection.insertOne(bike);
