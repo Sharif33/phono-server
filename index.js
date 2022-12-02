19b1d77aff27066f255cbc80cb4617bb2707b773
Sharif33 <sharif.cse.nu@gmail.cm>
2022-12-02 20:03:04 +0600

    vercel deploy fix

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(express.json());
 // jwt
 async function verifyToken(req,res,next){
 if(req.headers?.authorization?.startsWith('Bearer')){
-    const token = req.headers?.authorization?.split(' ')[1];
+    const token = req.headers.authorization.split(' ')[1];
 
     try {
         const decodedUser = await admin.auth().verifyIdToken(token);
